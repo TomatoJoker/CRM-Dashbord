269ca3ace301a434bf2ef1798f109f50f6626d50
Tomat <demid.yermak@marakas.digital>
2024-07-31 13:59:43 +0300

    Add mobile version

--- app/js/main.js
+++ app/js/main.js
@@ -11,3 +11,17 @@ function dynamicHeightToolbar() {
         document.documentElement.style.removeProperty('--vh');
     }
 } // calc window height without toolbar on mobile browser
+
+
+const mobMenu = document.querySelector('.js-mob-menu'),
+      btn = document.querySelector('.js-burger'),
+      isMenuOpen = btn.getAttribute('aria-expanded') === 'true' || false;
+btn.addEventListener('click', () => {
+
+    if (mobMenu.getAttribute('aria-expanded')) {
+        btn.setAttribute('aria-expanded', !isMenuOpen);
+    }
+
+    btn.classList.toggle('is-active');
+    mobMenu.classList.toggle('is-open');
+});
--- app/js/navigation.js
+++ app/js/navigation.js
@@ -1,14 +1,72 @@
 function pageWidget(pages) {
-    var widgetWrap = $('<div class="widget_wrap"><ul class="widget_list"></ul></div>');
-    widgetWrap.prependTo("body");
-    for (var i = 0; i < pages.length; i++) {
-        $('<li class="widget_item"><a class="widget_link" href="' + pages[i] + '.html' + '">' + pages[i] + '</a></li>').appendTo('.widget_list');
-    }
-    var widgetStilization = $('<style>body {position:relative} .widget_wrap{position:absolute;top:0;left:0;z-index:999999;padding:10px 20px;background:#222;border-bottom-right-radius:10px;-webkit-transition:all .3s ease;transition:all .3s ease;-webkit-transform:translate(-100%,0);-ms-transform:translate(-100%,0);transform:translate(-100%,0)}.widget_wrap:after{content:" ";position:absolute;top:0;left:100%;width:24px;height:24px;background:#222 url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAgMAAABinRfyAAAABGdBTUEAALGPC/xhBQAAAAxQTFRF////////AAAA////BQBkwgAAAAN0Uk5TxMMAjAd+zwAAACNJREFUCNdjqP///y/DfyBg+LVq1Xoo8W8/CkFYAmwA0Kg/AFcANT5fe7l4AAAAAElFTkSuQmCC) no-repeat 50% 50%;cursor:pointer}.widget_wrap:hover{-webkit-transform:translate(0,0);-ms-transform:translate(0,0);transform:translate(0,0)}.widget_item{padding:0 0 10px}.widget_link{color:#fff;text-decoration:none;font-size:15px;}.widget_link:hover{text-decoration:underline} </style>');
-    widgetStilization.prependTo(".widget_wrap");
+    var widgetWrap = document.createElement('div');
+    widgetWrap.className = 'widget_wrap';
+
+    var widgetList = document.createElement('ul');
+    widgetList.className = 'widget_list';
+    widgetWrap.appendChild(widgetList);
+
+    document.body.insertBefore(widgetWrap, document.body.firstChild);
+
+    pages.forEach(function(page) {
+
+        var listItem = document.createElement('li');
+        listItem.className = 'widget_item';
+
+        var link = document.createElement('a');
+        link.className = 'widget_link';
+        link.href = page + '.html';
+        link.textContent = page;
+
+        listItem.appendChild(link);
+        widgetList.appendChild(listItem);
+    });
+
+    var styles = `
+        body {position:relative} 
+        .widget_wrap {
+            position:absolute;
+            top:0;
+            left:0;
+            z-index:999999;
+            padding:10px 20px;
+            background:#222;
+            border-bottom-right-radius:10px;
+            transition:all .3s ease;
+            transform:translate(-100%, 0);
+        }
+        .widget_wrap:after {
+            content:" ";
+            position:absolute;
+            top:0;
+            left:100%;
+            width:24px;
+            height:24px;
+            background:#222 url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAgMAAABinRfyAAAABGdBTUEAALGPC/xhBQAAAAxQTFRF////////AAAA////BQBkwgAAAAN0Uk5TxMMAjAd+zwAAACNJREFUCNdjqP///y/DfyBg+LVq1Xoo8W8/CkFYAmwA0Kg/AFcANT5fe7l4AAAAAElFTkSuQmCC) no-repeat 50% 50%;
+            cursor:pointer;
+        }
+        .widget_wrap:hover {
+            transform:translate(0, 0);
+        }
+        .widget_item {
+            padding:0 0 10px;
+        }
+        .widget_link {
+            color:#fff;
+            text-decoration:none;
+            font-size:15px;
+        }
+        .widget_link:hover {
+            text-decoration:underline;
+        }
+    `;
+
+    var styleSheet = document.createElement("style");
+    styleSheet.type = "text/css";
+    styleSheet.innerText = styles;
+    document.head.appendChild(styleSheet);
 }
 
-
-$(function() {
+document.addEventListener("DOMContentLoaded", function() {
     pageWidget(['index']);
 });
